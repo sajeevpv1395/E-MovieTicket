b814c2fec5e0507be6d5222df1ea642f25e394bd
sajeev pv <sajeevpv1395@gmail.com>
2022-12-01 15:02:19 +0530

    Add project files.

---
+++ E-MovieTicket/Controllers/MoviesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_MovieTicket.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-MovieTicket/Controllers/OrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_MovieTicket.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
